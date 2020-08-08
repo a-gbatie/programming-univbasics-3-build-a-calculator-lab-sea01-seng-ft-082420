9eac3b2f9a360125fd7581ccf93b7780c76c5114
Ange Batie <ange.batie@gmail.com>
2020-08-08 00:16:50 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,11 +3,11 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  difference = 
+  difference = num1 - num2
 end
 
 def division(num1, num2)
-
+  quotient 
 end
 
 def multiplication(num1, num2)
